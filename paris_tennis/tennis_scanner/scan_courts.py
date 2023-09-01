ca02d9d7e4329e3b567391c2229477c77f11e6f9
hplaptop <7720585+nono-london@users.noreply.github.com>
2023-09-01 23:19:44 +0200

    Adding date scrapper

--- paris_tennis/tennis_scanner/scan_courts.py
+++ paris_tennis/tennis_scanner/scan_courts.py
@@ -1,10 +1,12 @@
 import asyncio
 from datetime import datetime
 from typing import List, Union, Optional
-
+from bs4 import BeautifulSoup as bs
+from numpy.f2py.symbolic import COUNTER
 from playwright.async_api import async_playwright, Page, Browser
 
 from paris_tennis.app_config import get_tennis_names
+from dataclasses import dataclass
 
 
 class ParisTennis:
@@ -16,6 +18,7 @@ class ParisTennis:
         self.browser: Union[Browser, None] = None
         self.page: Union[Page, None] = None
         self.day_checked: datetime = datetime(1900, 1, 1)
+        self.tennis_summaries: List[TennisCourtSummary] = list()
 
     async def start_browser(self):
         if not self.page:
@@ -41,10 +44,48 @@ class ParisTennis:
 
         submit_element = self.page.locator('xpath=//button[@id="rechercher"]')
         await submit_element.click(delay=100)
+
+    async def get_available_dates(self):
+        page_source = await self.page.content()
+        web_soup = bs(page_source, 'lxml')
+
+        court_dates = web_soup.find_all(name='div', attrs={'class': 'date-item'})
+        print(len(court_dates))
+        #
+        for court_date in court_dates:
+            tennis_court_summary = TennisCourtSummary()
+            # get court date
+            court_date_el = court_date.find(name='div', attrs={'class': 'date'})
+            if court_date_el:
+                court_date_str = court_date_el.get('dateiso')
+                if court_date:
+                    tennis_court_summary.tennis_date = datetime.strptime(court_date_str, '%d/%m/%Y')
+                print(tennis_court_summary.tennis_date)
+            # get number of courts available
+            court_number_el = court_date.find(name='div', attrs={'class': 'digits'})
+            if court_number_el:
+                court_number = court_number_el.get_text()
+                tennis_court_summary.available_courts = int(court_number)
+            self.tennis_summaries.append(tennis_court_summary)
+
+    async def check_all_availabilities(self):
+        await self.select_a_court()
+        await self.page.wait_for_timeout(5000)
+
+        await self.get_available_dates()
+        print(self.tennis_summaries)
+
         await self.page.wait_for_timeout(50000)
+
+
+
+@dataclass(repr=True)
+class TennisCourtSummary:
+    tennis_date: datetime = datetime(1900, 1, 1)
+    available_courts: int = 0
 
 
 if __name__ == '__main__':
     paris_tennis = ParisTennis()
 
-    asyncio.get_event_loop().run_until_complete(paris_tennis.select_a_court())
+    asyncio.get_event_loop().run_until_complete(paris_tennis.check_all_availabilities())
