13f6012e5e3a76964e0b6e0fb2cb67c374ae8a29
thinkcentre <7720585+nono-london@users.noreply.github.com>
2023-09-02 14:05:14 +0100

    added download folder

--- paris_tennis/app_config.py
+++ paris_tennis/app_config.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from typing import List
 
 
@@ -6,6 +7,23 @@ def get_tennis_names() -> List:
     return tennis_names
 
 
+def get_project_root_path() -> Path:
+    # https://stackoverflow.com/questions/5137497/find-the-current-directory-and-files-directory
+    root_dir = Path(__file__).resolve().parent.parent
+    return root_dir
+
+
+def get_project_download_path() -> Path:
+    # https://stackoverflow.com/questions/25389095/python-get-path-of-root-project-structure/40227116
+    root_path: Path = get_project_root_path()
+    download_folder_path = Path(root_path, root_path.name, "downloads")
+    if not download_folder_path.exists():
+        download_folder_path.mkdir(parents=True)
+    return download_folder_path
+
 
 if __name__ == '__main__':
     print(get_tennis_names())
+    print(get_project_root_path())
+    print(get_project_download_path())
+
